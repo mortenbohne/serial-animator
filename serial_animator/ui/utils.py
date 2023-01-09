77eedba27090ff9f5ff84e6a91a26e9af398d3dc
Morten Bøhne <github@mortenbohne.dk>
2023-01-09 10:05:42 +0100

    using pymel to get main window

--- serial_animator/ui/utils.py
+++ serial_animator/ui/utils.py
@@ -1,9 +1,5 @@
-import shiboken2
-from PySide2 import QtWidgets
-import maya.OpenMayaUI
+import pymel.core as pm
 
 
 def get_maya_main_window():
-    ptr = maya.OpenMayaUI.MQtUtil.mainWindow()
-    if ptr is not None:
-        return shiboken2.wrapInstance(int(ptr), QtWidgets.QWidget)
+    return pm.uitypes.toPySideControl(pm.MelGlobals.get("gMainWindow"))
