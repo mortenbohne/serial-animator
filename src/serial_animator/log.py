527df54a3627bcc8961274808520c6266955a4c1
mortenbohne <github@mortenbohne.dk>
2023-07-05 20:00:41 +0200

    Logger can be used as a context manager -hacks to make it work with pytest. Added pytests

--- src/serial_animator/log.py
+++ src/serial_animator/log.py
@@ -8,7 +8,7 @@ class LogLevelContextManager:
     def __init__(self, logger, level):
         self.logger = logger
         self.level = level
-        self.previous_level = logger.level
+        self.previous_level = logger.getEffectiveLevel()
 
     def __enter__(self):
         self.logger.setLevel(self.level)
@@ -17,26 +17,48 @@ class LogLevelContextManager:
         self.logger.setLevel(self.previous_level)
 
 
-class ContextLogger(logging.Logger):
-    def log_level_context(self, level):
-        return LogLevelContextManager(self, level)
+class LogAllLevelsContextManager:
+    def __init__(self, min_level=logging.CRITICAL):
+        self.min_level = min_level
+
+    def __enter__(self):
+        logging.disable(logging.CRITICAL)
+
+    def __call__(self, func):
+        """Allow class to be used as decorator"""
+
+        def decorator(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+
+        return decorator
+
+    def __exit__(self, exit_type, exit_value, exit_traceback):
+        logging.disable(logging.NOTSET)
 
 
 def log(name):
     """
     Simple stream-logger that can also be used as a context manager
     """
-    logger = ContextLogger(name)
-    # create console handler and set level to debug
+    logger = logging.getLogger(name)
+
+    # Hacks since current versions of pytest doesn't allow subclassing
+    # of logging.getLogger
+    setattr(logger, "all_log_levels", LogAllLevelsContextManager)
+
+    def at_level(level):
+        return LogLevelContextManager(logger, level)
+
+    setattr(logger, "at_level", at_level)
+    # end of pytest-hacks
     console_handler = logging.StreamHandler()
-    # create formatter
     formatter = logging.Formatter(
         "%(asctime)s-%(levelname)s:%(name)s.%(funcName)s: %(message)s", "%H:%M:%S"
     )
-    # add formatter to ch
     console_handler.setFormatter(formatter)
-    logger.propagate = 0
-    # add ch to logger
+    # keeping logger.propagate = 1 until pytests work with non-propagating logs
+    # logger.propagate = 0
     logger.handlers = [console_handler]
 
     return logger
---
+++ tests/serial_animator/test_log.py
@@ -0,0 +1,12 @@
+import logging
+from . import log_functions
+
+
+def test_log(caplog):
+    with caplog.at_level(logging.INFO):
+        log_functions.log_output()
+        assert "I'm info from log2" in caplog.text
+        assert "logger2 silenced" not in caplog.text
+        assert "logger info silenced" not in caplog.text
+        assert "Info from 2 still here" in caplog.text
+        assert "this debug message shouldn't be seen" not in caplog.text
